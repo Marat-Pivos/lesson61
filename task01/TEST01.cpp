a5ed09ab3fa819ade2b565ca76fcf2b135b24839
Pivos-Marat <pivovarevichmarat@gmail.com>
2026-06-09 18:00:57 +0300

    Добавьте файлы проекта.

---
+++ task01/TEST01.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//	int number = 10;
+//
+//	ptr = &number;
+//
+//	*ptr *= 2;
+//
+//	cout << &number <<" - " << number << "\n";
+//	cout << ptr << " - " << *ptr << "\n";
+//	cout << main << " - " << *main << "\n";
+//
+//	return 0;
+//}
